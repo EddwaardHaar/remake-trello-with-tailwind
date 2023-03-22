99ec99a673f026c660e87e7fd777ff2bf722d1af
Peter Harčar <pharcar7@gmail.com>
2023-03-22 21:41:59 +0200

    add responsivnes  & possibility to add new card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
-  purge: {
-		enabled: false,
-		content: [
-			"./dist/*.html",
-			"./src/**/*.vue",
-		]
-	},
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
