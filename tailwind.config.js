1767022aa225217cf7e6313deba8446097843c66
Peter Harčar <pharcar7@gmail.com>
2023-03-22 20:38:45 +0200

    add purge
    -during development i have purge disabled, but during production i can enable it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+  purge: {
+		enabled: false,
+		content: [
+			"./dist/*.html",
+			"./src/**/*.vue",
+		]
+	},
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
